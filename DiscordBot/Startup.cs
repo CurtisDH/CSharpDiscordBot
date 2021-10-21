a166050461a1b5fba4ffe1a5487788b978bd8822
CurtisDH <39945904+CurtisDH@users.noreply.github.com>
2021-10-21 18:24:03 +1000

    - config.yml is now created at runtime. If no token is provided within config user is prompted to enter one

--- DiscordBot/Startup.cs
+++ DiscordBot/Startup.cs
@@ -16,12 +16,21 @@ namespace DiscordBot
 
         public Startup(string[] args)
         {
+            string dir = AppContext.BaseDirectory + "/_config.yml";
+            if (!File.Exists(dir))
+            {
+                Program.Print("Warning!! Config file doesn't exist.. Creating one:"+dir);
+                File.WriteAllText(dir,"prefix: '!'" +
+                                      "\n" +
+                                      "tokens:\n" +
+                                      "discord: --Ensure this line is indented");
+            }
             var configBuilder = new ConfigurationBuilder()
                 .SetBasePath(AppContext.BaseDirectory)
                 .AddYamlFile("_config.yml");
             Configuration = configBuilder.Build();
         }
-
+        
         public static async Task RunAsync(string[] args)
         {
             var bot = new Startup(args);
--- DiscordBot/StartupService.cs
+++ DiscordBot/StartupService.cs
@@ -14,6 +14,7 @@ namespace DiscordBot
         private readonly DiscordSocketClient _client;
         private readonly CommandService _commands;
         private readonly IConfigurationRoot _config;
+        private string token;
 
         public StartupService(IServiceProvider provider,
             DiscordSocketClient client,
@@ -28,17 +29,52 @@ namespace DiscordBot
 
         public async Task StartAsync()
         {
-            string token = _config["tokens:discord"];
+            token = _config["tokens:discord"];
             if (string.IsNullOrEmpty(token))
             {
-                Console.WriteLine("Discord token empty..." +
-                                  " Please configure the _config.yml");
-                return;
+                ConfigureToken();
             }
 
-            await _client.LoginAsync(TokenType.Bot, token);
+            await VerifyTokenAndLogin();
             await _client.StartAsync();
             await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), Provider);
+        }
+
+        private async Task VerifyTokenAndLogin()
+        {
+            bool verified = false;
+            while (verified == false)
+            {
+                try
+                {
+                    await _client.LoginAsync(TokenType.Bot, token);
+                    verified = true;
+                    Console.WriteLine("Token accepted. Not saving to file");
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid Token:please enter a valid token");
+                    verified = false;
+                    ConfigureToken();
+                }
+            }
+        }
+
+        private void ConfigureToken()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter discord bot token, or configure the '_config.yml' manually and restart");
+                var input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    continue;
+                }
+
+                Console.WriteLine("Breaking");
+                token = input;
+                break;
+            }
         }
     }
 }
